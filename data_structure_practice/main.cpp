958554a85997f8bd9bd0f8ae3d9ddc9b8c25179a
JaydenLin <j5905653@gmail.com>
2014-12-14 22:13:15 +0800

    Initial Commit

---
+++ data_structure_practice/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  data_structure_practice
+//
+//  Created by jayden on 2014/12/14.
+//  Copyright (c) 2014年 jayden. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
